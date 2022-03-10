61db0797b60601d89f5176c91d8ffbd6cbeda8ce
MeherziKhouloud <khouloud.meherzi@metis-innovations.com>
2022-03-10 17:30:31 +0100

    add test

---
+++ src/test/java/com/esprit/examen/GesFApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esprit.examen;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class GesFApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
